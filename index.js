cc804835714d0750d07548cfae9034d250b9fdf0
Md.Ashrafull Islam <ashrafull130@gmail.com>
2022-11-25 21:51:21 +0600

    added category wise data load api and product details api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000 ;
 require('dotenv').config()
 
@@ -16,7 +16,6 @@ app.get('/', (req,res) => {
 })
 
 
-
 // Mongodb  section start here 
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rhjlmgh.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
@@ -25,12 +24,32 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const run = async() => {
  try{
   const categoryOptionCollection = client.db('computerResell').collection('category')
+  const productsCollection = client.db('computerResell').collection('categoryProducts')
    // get all category otion with products info 
    app.get('/categories', async(req,res) => {
     const query = {}
     const category = await categoryOptionCollection.find(query).toArray()
     res.send(category)
    })
+   
+   // get products under the category 
+   app.get('/categoryProducts/:id', async(req,res) => {
+    const id = req.params.id;
+    const query = {category_id:id}
+    const categoryProducts = await productsCollection.find(query).toArray()
+    res.send(categoryProducts)
+
+   })
+
+   // get specific products details by id 
+   app.get('/productDetails/:id', async(req,res) => {
+    const id = req.params.id ;
+    const query = {_id:ObjectId(id)}
+    const productDetails = await  productsCollection.find(query).toArray()
+    res.send(productDetails)
+
+   } )
+
  }
  finally{
    
